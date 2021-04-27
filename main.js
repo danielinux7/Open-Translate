2b466cfe02cfc4d838194715da4f658af1f1d270
danielinux7 <daniel.abzakh@gmail.com>
2021-04-27 13:35:38 +0000

    Deploying to gh-pages from  @ 2a3f9610c938def6f22e8bd657709279e93e2433 🚀

--- main.js
+++ main.js
@@ -197,6 +197,17 @@ const LANGS = [
     { id: 'ab', name: 'Аԥсуа' },
     { id: 'ru', name: 'Аурыс' }
     // { id: 'abq', name: 'Абаза' }
+    // { id: 'ru', name: 'Аурыс' },
+    // { id: 'abq', name: 'Абаза'},
+    // { id: 'ady', name: 'Адыгабзэ' },
+    // { id: 'kbd', name: 'къэбэрдейбзэ' },
+    // { id: 'dar', name: 'дарган мез' },
+    // { id: 'che', name: 'нохчийн мотт' },
+    // { id: 'ddo', name: 'цезйас мец' },
+    // { id: 'lez', name: 'лезги чӏал' },
+    // { id: 'inh', name: 'гӀалгӀай мотт' },
+    // { id: 'ava', name: 'Авар мацӏбаза' },
+    // { id: 'lbe', name: 'лакку маз' }
 ];
 
 
